973d61dc4d27d328e37707babbe95b54657a81c3
Mark Agimba <markaguero16@gmail.com>
2019-02-22 17:49:13 +0300

    varanswer for all 3 questions

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,10 @@ return "You scored" +parseInt((score / 75) * 100) + "%";
 
 $(document).ready(function(){
    $("#questions").submit(function (event) {
+
+
+      $('#result').text('');
+      var score =0;
+      var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
+      var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
+      var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
