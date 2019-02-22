f356fa03348ecb41e5e6cc31070306232e9958e1
Mark Agimba <markaguero16@gmail.com>
2019-02-22 17:44:04 +0300

    js submit function

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,6 @@ var pointPerCorrect = 25;
 function percentage(score) {
 return "You scored" +parseInt((score / 75) * 100) + "%";
 }
+
+$(document).ready(function(){
+   $("#questions").submit(function (event) {
