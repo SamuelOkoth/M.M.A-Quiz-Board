2b3150c88a345f23009ec1a1043ba8b27236a08f
Mark Agimba <markaguero16@gmail.com>
2019-02-22 18:00:31 +0300

    js line 20-31

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,23 @@ $(document).ready(function(){
 
 
       $('#result').text('');
-      var score =0;
-      var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
-      var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
-      var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+      var score = 0;
+        var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
+        var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
+        var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+
+        if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
+    $('#questionsIncomplete').text('You have not answered all the questions!');
+    $('#questionsIncomplete').fadeOut(9500);
+  } else {
+           if (answerOne === answers[0]) {
+      score += pointPerCorrect;
+    }
+           if (answerTwo === answers[1]) {
+   score += pointPerCorrect;
+    }
+           if (answerThree === answers[2]) {
+   score += pointPerCorrect;
+    }
+
+    
